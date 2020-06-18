0ce648b10178aaf4183bbd1c30653c7f2aaa5e4a
Piotr Krawczyk <piotr.krawczyk@tietoevry.com>
2020-06-18 18:11:40 +0200

    Use sqlite3 for sharing db.

--- vss-graphql/src/index.ts
+++ vss-graphql/src/index.ts
@@ -11,19 +11,47 @@ const runServer = async (): Promise<void> => {
     port = Number.isNaN(parsedPort) ? defaultPort : parsedPort;
   }
 
-  const low = require('lowdb');
-  const FileSync = require('lowdb/adapters/FileSync');
-
-  const adapter = new FileSync('/usr/shared/db/db.json');
-  const db = low(adapter);
+  const sqlite3 = require("sqlite3");
+  const dbPath = "/usr/shared/db/opends.db";
+  let sqlite_db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the opends database.');
+  });
 
   const resolvers = {
     Query: {
       vehicle(parent, args, context, info) {
-        db.read();
-        const veh = db.get('vehicle').value()
-        console.log(JSON.stringify(veh));
-        return veh;
+        let key = "";
+        let object = new Object();
+        let root = new Object();
+
+        let promise = new Promise((resolve, reject) => {
+          sqlite_db.each("SELECT key, value FROM vss_data", (err, row) => {
+            key = row.key;
+            let leafs = key.split('.');
+            let leaf = leafs.pop();
+            object = root;
+            for (let elem of leafs) {
+              if (object !== undefined) {
+                if (!(elem in object)) {
+                  object[elem] = new Object();
+                }
+                object = object[elem];
+              }
+            }
+            if (object !== undefined && leaf !== undefined) object[leaf] = row.value;
+          }, (err, n) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(root);
+            }
+          });
+        });
+
+        return promise;
       }
     }
   }
